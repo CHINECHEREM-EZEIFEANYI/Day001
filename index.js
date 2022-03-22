b85f28c0921e951ea3bc2aa8099adab4050e0c1d
CHINECHEREM-EZEIFEANYI <82101590+CHINECHEREM-EZEIFEANYI@users.noreply.github.com>
2022-03-22 14:18:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var x; //indicating what variable to use
+    console.log(`Even numbers from 2 to 10 are:`)
+//using for loop for iteration
+for (x = 2; x <= 10; x++) { 
+  if (x % 2 == 0) { //conditional statement
+ console.log(`${x}`);
+}
+}
+
+
+
+
+
